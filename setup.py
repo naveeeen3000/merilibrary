c2b25c7d3fd2154ee9fd36ea5786eed5402c9ec2
naveen <naveen@playo.co>
2023-10-18 15:13:00 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='merilibrary',
+    version='0.1.0',
+    description='Ye hai meri python library',
+    url='/Files/Pytorch-RL',
+    author='Test'
+)
